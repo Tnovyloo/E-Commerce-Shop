89d0a460d2af3d183bf019fe661433b2812d6628
TnovyLoo <loobrome@gmail.com>
2022-12-05 21:05:09 +0100

    Creating own super user model.

---
+++ accounts/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import *
+from django.contrib.auth.admin import UserAdmin
+
+
+# Register your models here.
+
+class AccountAdmin(UserAdmin):
+    """ This class changes "Accounts" page appearance, it's more visibility and clear
+     because it creates 'columns' and 'rows',
+     it also creates django custom user model. """
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
+    list_display_links = ('email', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
+    ordering = ('-date_joined', )
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(Account, AccountAdmin)
