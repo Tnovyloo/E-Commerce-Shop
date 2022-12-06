8b08587126988c342b3959739ec04412f4313dda
TnovyLoo <loobrome@gmail.com>
2022-12-06 20:16:33 +0100

    Creating products model, initializing models in admin site, amending and updating templates.

--- category/models.py
+++ category/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=50, unique=True)
-    slug = models.CharField(max_length=100, unique=True)
-    description = models.TextField(max_length=255)
-    category_image = models.ImageField(upload_to='media/categories', blank=True)
+    category_name       = models.CharField(max_length=50, unique=True)
+    slug                = models.SlugField(max_length=100, unique=True)
+    description         = models.TextField(max_length=255)
+    category_image      = models.ImageField(upload_to='media/categories', blank=True)
 
     class Meta:
         verbose_name = 'category'
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from store.models import Product
 
 def home(request):
-    # return HttpResponse("Homepage!")
-    return render(request, 'home.html')
+    products = Product.objects.all().filter(is_available=True)
+
+    context = {
+        'products': products,
+
+    }
+
+    return render(request, template_name='home.html', context=context)
 
