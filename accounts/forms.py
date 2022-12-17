3b1c0f99d3951c382e1c936c0f2a04dee31beb73
TnovyLoo <loobrome@gmail.com>
2022-12-17 14:44:40 +0100

    Creating django auth form and adjusting the html to this form.

---
+++ accounts/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Account
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+        'class': 'form-control',
+    }
+    ))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password',
+        'class': 'form-control',
+    }
+    ))
+
+    class Meta:
+        model = Account
+        fields = ["first_name", "last_name", "phone_number",
+                  "email", "password"]
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter your First name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter your Last name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter your phone number'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter your E-mail address'
+
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
