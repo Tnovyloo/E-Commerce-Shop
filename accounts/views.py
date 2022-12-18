8c51934299531afdc3749d4db0b19d5c977f91a1
TnovyLoo <loobrome@gmail.com>
2022-12-18 14:38:28 +0100

    Creating if form is valid.

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from .forms import RegistrationForm
-
+from accounts.models import Account
 # Create your views here.
 def register(request):
-    form = RegistrationForm
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid(): # Check if the form is valid
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            phone_number = form.cleaned_data["phone_number"]
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+
+            user = Account.objects.create_user
+            # TODO 3:12
+
     context = {
         'form': form,
     }
