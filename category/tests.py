078db6c36ac99dcf268c8a4db66b5d0c6a6b8b7b
TnovyLoo <loobrome@gmail.com>
2022-12-25 21:06:17 +0100

    Creating simple tests.

--- category/tests.py
+++ category/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from category.models import Category
 
 # Create your tests here.
+class CategoryTestCase(TestCase):
+    def create_category(self, name="Test category", desc="Lorem ipsum dipsum lipsum ipusm trum trum", slug="test-category"):
+        return Category.objects.create(category_name=name,
+                                       slug=slug,
+                                       description=desc)
+
+    def test_category_object(self):
+        obj = self.create_category()
+        self.assertTrue(isinstance(obj, Category))
+        self.assertEqual(obj.__str__(), obj.category_name)
--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
-
+from django.test import TestCase, SimpleTestCase, Client
+from django.urls import reverse, resolve
+from .views import payments, place_order
+from .models import Payment, Product
+from accounts.models import Account
+import json
 # Create your tests here.
+
+class TestUrls(SimpleTestCase):
+
+    def test_payments_url(self):
+        url = reverse('payments')
+        self.assertEquals(resolve(url).func, payments)
+
+    def test_place_order(self):
+        url = reverse('place_order')
+        self.assertEquals(resolve(url).func, place_order)
+
+
+# class TestViews(TestCase):
+
+#     def test_place_order_view(self):
+#         url = reverse("place_order")
+#         # resp = client.get(url)
+#         user = Account.objects.create(username='testuser')
+#         user.set_password('123')
+#         user.save()
+#
+#         client = Client()
+#         client.login(username='testuser', password='123')
+#
+#         response = client.get(url)
+#         self.assertEqual(response.status_code, 200)
