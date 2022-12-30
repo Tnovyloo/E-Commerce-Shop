488cba4ace32355533cfc55a05a2f2486afad7a6
TnovyLoo <loobrome@gmail.com>
2022-12-30 15:38:24 +0100

    Small fixes with pagination etc.

--- shop/settings.py
+++ shop/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+# Creating env variables.
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zs1r*2-%n@rb&v&fhev0%no1y-+o4ed08sj3&(pbkcu+v(6j4&'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -151,10 +157,10 @@ MESSAGE_TAGS = {
 }
 
 # SMTP configuration
-import environ
+# import environ
 # Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
+# env = environ.Env()
+# environ.Env.read_env()
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
