a0901d38712132b4c7ac03817f751d4ff228fcb4
TnovyLoo <loobrome@gmail.com>
2023-01-01 20:26:45 +0100

    Fixes.

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('', store, name='store'),
     path('category/<slug:category_slug>/', store, name="products_by_category"),
-    path('category/ <slug:category_slug>/<slug:product_slug>', product_detail, name="product_detail"),
+    path('category/<slug:category_slug>/<slug:product_slug>', product_detail, name="product_detail"),
     path('search/', search, name='search'),
     path('submit_review/<int:product_id>/', submit_review, name="submit_review")
 ]
